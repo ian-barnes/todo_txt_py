9205596b8ad9028f1315c2d079de94af2957544f
Sébastien Geay <sebastien@cryptosense.com>
2022-06-09 17:10:01 +0200

    Session 2

--- tests/test_todo_txt.py
+++ tests/test_todo_txt.py
@@ -15,8 +15,22 @@ def setup_function(pytester: pytest.Pytester):
 
 def test_list():
     runner = CliRunner()
-    result = runner.invoke(app.list)
+    result = runner.invoke(app.list, ['--filter', '@'])
+    lines = filter(lambda line: line, result.output.split('\n'))
+    assert all('@' in line for line in lines)
+    assert result.exit_code == 0
 
+
+def test_report():
+    runner = CliRunner()
+    result = runner.invoke(app.report)
+    expected = '''\
+5 tasks, 1 completed (20.0%)
+Task counts by priority:
+(A) -> 1 tasks, 0 completed
+(B) -> 1 tasks, 0 completed
+'''
+    assert result.output == expected
     assert result.exit_code == 0
 
 
